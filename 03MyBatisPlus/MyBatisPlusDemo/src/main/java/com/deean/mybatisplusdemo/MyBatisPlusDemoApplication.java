da45cd04c0bd93f229027f8e363950ce1fab24d8
spriteboysz <sprite_bx@126.com>
2023-11-23 22:56:55 +0800

    2023112322

---
+++ 03MyBatisPlus/MyBatisPlusDemo/src/main/java/com/deean/mybatisplusdemo/MyBatisPlusDemoApplication.java
@@ -0,0 +1,13 @@
+package com.deean.mybatisplusdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisPlusDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisPlusDemoApplication.class, args);
+    }
+
+}
---
+++ 03MyBatisPlus/MyBatisPlusDemo/src/test/java/com/deean/mybatisplusdemo/MyBatisPlusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deean.mybatisplusdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisPlusDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
