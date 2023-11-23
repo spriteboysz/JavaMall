3409d92bddaac84338248f312e1164e14fd30299
spriteboysz <sprite_bx@126.com>
2023-11-23 23:00:44 +0800

    2023112323

---
+++ 02TKMapper/TKMapperDemo/src/main/java/com/deean/tkmapperdemo/TkMapperDemoApplication.java
@@ -0,0 +1,13 @@
+package com.deean.tkmapperdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkMapperDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TkMapperDemoApplication.class, args);
+    }
+
+}
---
+++ 02TKMapper/TKMapperDemo/src/test/java/com/deean/tkmapperdemo/TkMapperDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deean.tkmapperdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkMapperDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
