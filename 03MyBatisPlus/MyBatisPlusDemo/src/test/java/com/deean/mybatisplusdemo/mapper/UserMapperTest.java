b629c8fdb1692dced96d8b479e1772bac4486f7d
spriteboysz <sprite_bx@126.com>
2023-11-24 22:18:53 +0800

    2023112422

--- 03MyBatisPlus/MyBatisPlusDemo/src/test/java/com/deean/mybatisplusdemo/mapper/UserMapperTest.java
+++ 03MyBatisPlus/MyBatisPlusDemo/src/test/java/com/deean/mybatisplusdemo/mapper/UserMapperTest.java
@@ -4,11 +4,12 @@ import com.deean.mybatisplusdemo.bean.User;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.Extension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -24,6 +25,20 @@ class UserMapperTest {
         List<User> users = userMapper.selectList(null);
         assertNotNull(users);
         users.forEach(System.out::println);
+    }
+
+    @Test
+    public void queryTest02() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("user_name", "Deean");
+        List<User> users = userMapper.selectByMap(map);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void queryTest03() {
+        Long num = userMapper.selectCount(null);
+        System.out.println(num);
     }
 
 }
