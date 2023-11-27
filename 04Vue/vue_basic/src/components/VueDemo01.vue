eb970fe0f9b99319881f3f8226a6e24db4dbe84f
spriteboysz <sprite_bx@126.com>
2023-11-27 23:32:06 +0800

    2023112723

---
+++ 04Vue/vue_basic/src/components/VueDemo01.vue
@@ -0,0 +1,23 @@
+<template>
+    <h3>Demo01: 文本插值</h3>
+    <p>字符串: {{ msg }}</p>
+    <p>数值加减: {{ num + 1 }}</p>
+    <p>三目运算：{{ ok ? "Yes" : "No" }}</p>
+    <p>字符串处理：{{ ss.split("").reverse().join("") }}</p>
+    <p v-html="rawHtml"></p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "Hello World!",
+            num: 10,
+            ok: true,
+            ss: "大家好",
+            rawHtml: "<a herf='https://www.baidu.com'>百度</a>"
+        }
+    }
+}
+</script>
+
---
+++ 04Vue/vue_basic/src/components/VueDemo02.vue
@@ -0,0 +1,31 @@
+<template>
+    <h3>Demo02: 属性绑定</h3>
+    <div :id="dynamicId" :class="dynamicClass" :title="dynamicTitle">测试</div>
+    <button :disabled="dynamicButton">Button</button>
+    <div v-bind="ObjectOfAttrs">测试多属性绑定</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            dynamicClass: "appClass",
+            dynamicId: "appId",
+            dynamicTitle: null,
+            dynamicButton: false,
+            ObjectOfAttrs: {
+                class: "appClass",
+                id: "appId"
+            }
+        }
+    }
+}
+</script>
+
+
+<style>
+.appClass {
+    color: red;
+    font-size: 30px;
+}
+</style>
