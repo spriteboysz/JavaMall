a50c5931ffff76d978bb79204af9b55a70323bea
spriteboysz <sprite_bx@126.com>
2023-11-28 22:59:02 +0800

    2023112822

---
+++ 04Vue/vue_basic_02/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Demo_event_01 from "@/components/demo_event_01.vue";
+</script>
+
+<template>
+<demo_event_01/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ 04Vue/vue_basic_02/src/components/demo_event_01.vue
@@ -0,0 +1,26 @@
+<template>
+    <h3>Demo Event 01: 事件参数</h3>
+    <button @click="addCount">Add</button>
+    <p>{{ count }}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        addCount(e) {
+            e.target.innerHTML = "Add " + this.count
+            this.count++
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
