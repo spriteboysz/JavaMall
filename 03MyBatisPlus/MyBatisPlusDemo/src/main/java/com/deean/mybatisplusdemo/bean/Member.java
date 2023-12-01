a32de3430711612a55b3ed85da7742b4e71db038
spriteboysz <sprite_bx@126.com>
2023-12-01 23:23:35 +0800

    2023120123

--- 03MyBatisPlus/MyBatisPlusDemo/src/main/java/com/deean/mybatisplusdemo/bean/Member.java
+++ 03MyBatisPlus/MyBatisPlusDemo/src/main/java/com/deean/mybatisplusdemo/bean/Member.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.io.Serial;
 import java.io.Serializable;
 import lombok.Data;
 
@@ -40,6 +42,7 @@ public class Member implements Serializable {
      */
     private String memberCity;
 
+    @Serial
     @TableField(exist = false)
     private static final long serialVersionUID = 1L;
 }
--- 03MyBatisPlus/MyBatisPlusDemo/src/test/java/com/deean/mybatisplusdemo/generator/CodeGenerationTests.java
+++ 03MyBatisPlus/MyBatisPlusDemo/src/test/java/com/deean/mybatisplusdemo/generator/CodeGenerationTests.java
@@ -35,27 +35,25 @@ public class CodeGenerationTests {
                             .pathInfo(Collections.singletonMap(OutputFile.xml, System.getProperty("user.dir") + "/src/main/resources/mappers")); // 设置mapperXml生成路径
                 })
                 // 策略配置
-                .strategyConfig(builder -> {
-                            builder.addInclude("sys_menu") // 设置需要生成的表名
-                                    .addTablePrefix("sys_") // 设置过滤表前缀
-                                    // Entity 策略配置
-                                    .entityBuilder()
-                                    .enableLombok() //开启 Lombok
-                                    .enableFileOverride() // 覆盖已生成文件
-                                    .naming(NamingStrategy.underline_to_camel)  //数据库表映射到实体的命名策略：下划线转驼峰命
-                                    .columnNaming(NamingStrategy.underline_to_camel)    //数据库表字段映射到实体的命名策略：下划线转驼峰命
-                                    // Mapper 策略配置
-                                    .mapperBuilder()
-                                    .enableFileOverride() // 覆盖已生成文件
-                                    // Service 策略配置
-                                    .serviceBuilder()
-                                    .enableFileOverride() // 覆盖已生成文件
-                                    .formatServiceFileName("%sService") //格式化 service 接口文件名称，%s进行匹配表名，如 UserService
-                                    .formatServiceImplFileName("%sServiceImpl") //格式化 service 实现类文件名称，%s进行匹配表名，如 UserServiceImpl
-                                    // Controller 策略配置
-                                    .controllerBuilder()
-                                    .enableFileOverride();
-                        }
+                .strategyConfig(builder -> builder.addInclude("sys_menu") // 设置需要生成的表名
+                        .addTablePrefix("sys_") // 设置过滤表前缀
+                        // Entity 策略配置
+                        .entityBuilder()
+                        .enableLombok() //开启 Lombok
+                        .enableFileOverride() // 覆盖已生成文件
+                        .naming(NamingStrategy.underline_to_camel)  //数据库表映射到实体的命名策略：下划线转驼峰命
+                        .columnNaming(NamingStrategy.underline_to_camel)    //数据库表字段映射到实体的命名策略：下划线转驼峰命
+                        // Mapper 策略配置
+                        .mapperBuilder()
+                        .enableFileOverride() // 覆盖已生成文件
+                        // Service 策略配置
+                        .serviceBuilder()
+                        .enableFileOverride() // 覆盖已生成文件
+                        .formatServiceFileName("%sService") //格式化 service 接口文件名称，%s进行匹配表名，如 UserService
+                        .formatServiceImplFileName("%sServiceImpl") //格式化 service 实现类文件名称，%s进行匹配表名，如 UserServiceImpl
+                        // Controller 策略配置
+                        .controllerBuilder()
+                        .enableFileOverride()
                 ).execute();
     }
 }
