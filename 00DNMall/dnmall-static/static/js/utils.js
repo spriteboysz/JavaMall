8caf4b36cd58e9d59bc503bae5e5ff12300f4a22
spriteboysz <sprite_bx@126.com>
2023-12-20 00:11:05 +0800

    2023122000

--- 00DNMall/dnmall-static/static/js/utils.js
+++ 00DNMall/dnmall-static/static/js/utils.js
@@ -1,11 +1,11 @@
 function getUrlParam(key){
-	var url = decodeURI( window.location.toString() );
-	var arr = url.split("?");
+	const url = decodeURI(window.location.toString());
+	const arr = url.split("?");
 	if(arr.length>1){
-		var params = arr[1].split("&");
-		for(var i=0; i<params.length; i++){
-			var param = params[i];  //"pid=101"
-			if(param.split("=")[0] == key ){
+		const params = arr[1].split("&");
+		for(let i=0; i<params.length; i++){
+			const param = params[i];  //"pid=101"
+			if(param.split("=")[0] === key ){
 				return param.split("=")[1];
 			}
 		}
