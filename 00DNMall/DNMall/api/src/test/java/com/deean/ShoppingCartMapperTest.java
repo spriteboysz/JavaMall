bfc1dfe264a24a999aa182cea54c5c2a2ffc8a40
spriteboysz <sprite_bx@126.com>
2023-12-28 23:38:30 +0800

    2023122823

---
+++ 00DNMall/DNMall/api/src/test/java/com/deean/ShoppingCartMapperTest.java
@@ -0,0 +1,40 @@
+package com.deean;
+
+import com.deean.dnmall.bean.ShoppingCartVO;
+import com.deean.dnmall.mapper.ShoppingCartMapper;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+/**
+ * Author: Deean
+ * Date: 2023-12-28 23:32
+ * FileName: api/src/test/java/com/deean
+ * Description:
+ */
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class ShoppingCartMapperTest {
+
+    @Resource
+    private ShoppingCartMapper shoppingCartMapper;
+
+    @Test
+    public void listShoppingCartTest() {
+        List<ShoppingCartVO> shoppingCartVOS = shoppingCartMapper.selectCartByUserId(1);
+        for (ShoppingCartVO shoppingCartVO : shoppingCartVOS) {
+            System.out.println(shoppingCartVO);
+        }
+    }
+
+    @Test
+    public void updateShoppingCartTest() {
+        int i = shoppingCartMapper.updateCartNumByCartId(5, 10);
+        System.out.println(i);
+    }
+}
